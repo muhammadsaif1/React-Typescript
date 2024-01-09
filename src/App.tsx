bafcf96292c7070d9a06edf390d05649dca7a696
Saif <saif.shabir12@gmail.com>
2024-01-10 01:19:33 +0500

    changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Todos from './components/Todos';
+import Todo from './models/todo';
 
-import './App.css';
 
 function App() {
+  const todos =[
+    new Todo('Learn React'),
+    new Todo('Learn Typescript'),
+  ]
   return (
     <div >
+      <Todos items={todos}/>
      
     </div>
   );
